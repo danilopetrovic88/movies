cf5653c95629032c550e27366b4c80f67ead88bb
danilopetrovic88 <vivify@VivifyIdeass-MacBook-Pro-3.local>
2022-05-24 18:56:29 +0200

    napravljen model Movie,MovieController i movies view

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -405,7 +405,22 @@
 </head>
 
 <body class="antialiased">
-
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+                @foreach($movies as $movie)
+                <div class="card">
+                    <div class="card-header">
+                        <a href="/movies/{{ $movie->id }}" class="nav-link">{{ $movie->title }}</a>
+                    </div>
+                    <div class="card-body">
+                        <p class="p-2">{{ $movie->storyline }}</p>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
